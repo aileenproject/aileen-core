258e2cd71af6596ed623af5856b3e42c009443f5
ericcbonet <ericcbonet@gmail.com>
2019-04-15 16:37:24 +0000

    add CORS middleware

--- aileen/aileen/settings.py
+++ aileen/aileen/settings.py
@@ -32,18 +32,22 @@ INSTALLED_APPS = [
     "bootstrap3",
     "leaflet",
     "djgeojson",
+    "corsheaders",
     "data",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "aileen.urls"
 
